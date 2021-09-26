438cb07312213468c4142a42cbae74ecf7daa2a8
saperez17 <saperez@unicauca.edu.co>
2021-09-25 19:54:01 -0500

    excercise 7.8 ultimate hooks. Implemented the useResource custom hook to fetch resources from the backend

--- src/App.js
+++ src/App.js
@@ -20,10 +20,13 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
-
+  useEffect(()=>{
+    axios.get(baseUrl)
+    .then( res => setResources(res.data))
+  },[])
   const create = (resource) => {
-    // ...
+    axios.post(baseUrl, resource)
+    .then(res => setResources(preVal => ([...preVal, res.data])))
   }
 
   const service = {
